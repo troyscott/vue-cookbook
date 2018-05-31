3de4b0f4d08caf381e848bc7fce49f7c62bd3919
Troy Scott <consult@troyscott.ca>
2018-05-31 15:38:36 -0700

    add a simple app component which calculates investment gains

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,42 @@
 <template>
   <div id="app">
-  <h1>My App</h1> 
+  <div>
+    <label>principal capital</label>
+    <input v-model.number="principal">
+  </div>
+  <div>
+    <label>Yearly interestRate</label>
+    <input v-model.number="interestRate">
+  </div>
+  <div>
+    <label>Investment length (timeYears)</label>
+    <input v-model.number="timeYears">
+  </div>
+  <div>
+    You will gain:
+    <output>{{final}}</output>
+  </div>
   </div>
 </template>
+
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      principal: 0,
+      interestRate: 0,
+      timeYears: 0
+    }
+  },
+  computed: {
+    final () {
+      const P = this.principal
+      const r = this.interestRate
+      const t = this.timeYears
+      return P * Math.pow((1 + r), t)
+    }
+  }
 }
 </script>
 <style>
